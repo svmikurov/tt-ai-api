32ee9f69841b78de546ababb281167c5890ddeeb
Sergei Mikurov <454004@mail.ru>
2026-08-14 19:15:08 +0500

    feat(entrypoint): Add db check

--- src/sd/entrypoint/main.py
+++ src/sd/entrypoint/main.py
@@ -1,8 +1,13 @@
 """FastAPI entrypoint."""
 
 from fastapi import FastAPI
+from sqlalchemy import create_engine, text
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 app = FastAPI(title='Demo API', version='0.1.0')
+engine = create_engine(DATABASE_URL)
 
 
 @app.get('/')
@@ -13,3 +18,10 @@ async def root():
 @app.get('/health')
 async def health():
     return {'status': 'ok'}
+
+
+@app.get('/db-check')
+async def db_check():
+    with engine.connect() as conn:
+        result = conn.execute(text('SELECT 1'))
+        return {"status": "connected", "result": result.scalar()}
